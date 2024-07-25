72947a4b7c7b9386930e18368a5ce58f451522b6
Satyam Sharma <satyamshharish@gmail.com>
2024-07-26 01:19:08 +0530

    create string Calculator Service and check service instance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StringCalculatorService } from './string-calculator.service';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private stringCalcService : StringCalculatorService){  }
 
   title = 'String Calculator TDD'; 
   input:any = '';
