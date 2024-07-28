74185d36384c41c25ef660190a45e0a210ad0279
Satyam Sharma <satyamshharish@gmail.com>
2024-07-28 19:19:16 +0530

    handle BigInt number with calc

--- src/app/string-calculator.service.ts
+++ src/app/string-calculator.service.ts
@@ -8,16 +8,42 @@ export class StringCalculatorService {
 
   constructor() { }
 
-  add(numbers: string): Observable<number> {
+  add(numbers: string): Observable<string | BigInt> {
     if (!numbers) {
-      return of(0);
+      return of('0');
     }
 
     let delimiters = [','];
 
-    let nums = numbers.split(new RegExp(`[${delimiters.join('')}]`)).map(num => num?parseFloat(num):0);
-    const total = nums.reduce((sum, num) => sum + num, 0)
-    return of(Number(total));
+    const total = this.NumberHandler(numbers,delimiters)
+    return of(total);
     
   }
+
+  NumberHandler(numbers: string,delimiters:string[]):string{
+
+        let nums = numbers.split(new RegExp(`[${delimiters.join('')}]`));
+        const total = this.sumDecimalsAndNumbers(nums);
+        return total
+  }
+
+  isDecimal(number:string|number|BigInt):boolean{
+      return (number).toString().includes('.');
+  }
+  
+  sumDecimalsAndNumbers(numbers:string[]):string{
+    let decimals = numbers.filter((val)=>this.isDecimal(val));
+    let integertotal = numbers.filter((val)=>!(this.isDecimal(val))).reduce((sum, num) =>BigInt(sum) + BigInt(num), BigInt(0));
+
+    let total = '0';
+
+    if(decimals.length){
+     let decimaltotal = decimals.reduce((sum, num) => sum + (num?parseFloat(num):0),0).toString().split('.');
+      total = (integertotal + BigInt(decimaltotal[0])).toString() + '.'+ decimaltotal[1]
+    }else{
+      total = (integertotal).toString();
+    }
+
+    return total;
+}
 }
